f0767c88b7f9427bd3f9b7c85ad5f14919a286f8
nuriaandresfoto <nuriaandresfoto@gmail.com>
2021-07-15 13:28:48 +0200

    Add carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- default gallery mode -->
+  <v-gallery type="carousel" :images="list"  >
+  </v-gallery>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    data(){
+    return {
+      list: [
+        {title:'Image1',url:'https://images2.alphacoders.com/642/642540.png'},
+        {title:'Image2',url:'https://images3.alphacoders.com/633/633288.png'},
+        {title:'Image3',url:'https://images6.alphacoders.com/633/633294.png'},
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vGallery from 'v-gallery'
+Vue.use(vGallery)
 
 Vue.config.productionTip = false
 
