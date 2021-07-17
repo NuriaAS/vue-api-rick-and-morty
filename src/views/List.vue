1143c79c6e47d2fb0acc4e10bb87559530bc40f6
nuriaandresfoto <nuriaandresfoto@gmail.com>
2021-07-17 12:51:08 +0200

    Get and display first character page

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an List page</h1>
+  <div class="list">
+    <h1>This is a List page</h1>
+    <ul>
+      <li v-for="character in characters" :key="character.id">
+        <img :src="character.image" alt="">
+        <h2>{{ character.name }}</h2>
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default ({
+  data() {
+    return {
+      characters: [],
+    }
+  },
+  mounted() {
+    axios.get(`https://rickandmortyapi.com/api/character`)
+      .then( response => {
+        const data = response.data;
+        this.characters= data.results;
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+})
+</script>
+
