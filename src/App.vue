217cb2416a6b74ff73a8846eeec3606f0eb03197
nuriaandresfoto <nuriaandresfoto@gmail.com>
2021-08-18 13:32:47 +0200

    Add transitions

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
       <Header/>
       <main>
-        <router-view/>
+        <transition name="fade">
+          <router-view/>
+        </transition> 
       </main>
       <footer>
         <h4>This is the footer</h4>
@@ -20,6 +22,9 @@ export default ({
 </script>
 
 <style lang="scss">
+html {
+  min-height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -39,5 +44,28 @@ export default ({
       color: #42b983;
     }
   }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease;
+}
+.fade-enter-active {
+  transition-delay: 0.5s;
+}
+.fade-enter {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+.fade-enter-to {
+  opacity: 1;
+  transform: translateY(0px);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(100px);
+}
+.fade-leave {
+  opacity: 1;
+  transform: translateY(0px);
 }
 </style>
